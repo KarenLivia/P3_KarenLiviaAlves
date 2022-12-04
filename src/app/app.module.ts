f246be154414d0007fe112fed34dfe7a4dd55b16
KarenLivia <55704139+KarenLivia@users.noreply.github.com>
2022-12-04 18:37:11 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AgendamentosComponent } from './agendamentos/agendamentos.component';
+import { CadastroProfissionalComponent } from './cadastro-profissional/cadastro-profissional.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { SairComponent } from './sair/sair.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {path: "agendamentos", component: AgendamentosComponent},
+      {path: "cadastro-profissional", component: CadastroProfissionalComponent},
+      {path: "cadastro-usuario", component: CadastroUsuarioComponent},
+      {path: "", component: HomeComponent},
+      {path:"sair", component: SairComponent},
+    ])
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent ],
+  bootstrap:    [ 
+    AppComponent ]
 })
 export class AppModule { }
