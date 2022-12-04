703eedae478029849822c3ac763996af8875472d
KarenLivia <55704139+KarenLivia@users.noreply.github.com>
2022-12-04 19:01:35 -0300

    1 file modified, 21 added, 21 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AgendamentosComponent } from './agendamentos/agendamentos.component';
-import { CadastroProfissionalComponent } from './cadastro-profissional/cadastro-profissional.component';
-import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
-import { SairComponent } from './sair/sair.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SobreComponent } from './sobre/sobre.component';
-
+import { HomeComponent } from './componentes/home/home.component';
+import { AgendamentosComponent } from './componentes/agendamentos/agendamentos.component';
+import { CadastroProfissionalComponent } from './componentes/cadastro-profissional/cadastro-profissional.component';
+import { CadastroUsuarioComponent } from './componentes/cadastro-usuario/cadastro-usuario.component';
+import { SairComponent } from './componentes/sair/sair.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { SobreComponent } from './componentes/sobre/sobre.component';
 
 @NgModule({
   imports:      [ 
