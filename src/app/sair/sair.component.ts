4d6462195856645e6e00f5b5cf632bf159ed3cd5
KarenLivia <55704139+KarenLivia@users.noreply.github.com>
2022-12-04 18:32:35 -0300

    2 files modified, 5 added

---
+++ src/app/sair/sair.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sair',
+  templateUrl: './sair.component.html',
+  styleUrls: ['./sair.component.css']
+})
+export class SairComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
