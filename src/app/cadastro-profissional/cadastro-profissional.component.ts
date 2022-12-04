e01707e89b4c7b5524991ec505d23a774843b60e
KarenLivia <55704139+KarenLivia@users.noreply.github.com>
2022-12-04 18:29:04 -0300

    8 files added

---
+++ src/app/cadastro-profissional/cadastro-profissional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-profissional',
+  templateUrl: './cadastro-profissional.component.html',
+  styleUrls: ['./cadastro-profissional.component.css']
+})
+export class CadastroProfissionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
