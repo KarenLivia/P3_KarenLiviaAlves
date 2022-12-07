38fa5265b02e557d7a2fe61260007c4f0e6f21a6
KarenLivia <55704139+KarenLivia@users.noreply.github.com>
2022-12-06 22:01:35 -0300

    2 files modified

--- src/app/services/cadastro-cliente.service.ts
+++ src/app/services/cadastro-cliente.service.ts
@@ -2,13 +2,11 @@ import { Injectable } from '@angular/core';
 
 class cadastroCliente {
   name: string;
- // cell: number;
 }
 
 @Injectable()
 export class CadastroClienteService {
   private nameC: cadastroCliente[] = [];
- // private cellphoneC: cadastroCliente[] = [];
 
   constructor() {}
 
@@ -21,14 +19,4 @@ export class CadastroClienteService {
   getList() {
     return this.nameC;
   }
-/*
-  add1(cell: number) {
-    this.cellphoneC.push({ cell });
-  }
-  remove1(index: number) {
-    this.cellphoneC.splice(index, 1);
-  }
-  getList1() {
-    return this.cellphoneC;
-  }*/
 }
